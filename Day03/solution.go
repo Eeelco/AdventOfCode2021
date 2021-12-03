878dc964c8c95a7b50c2c258ea94c3e2ee79c8d7
Eelco Empting <me@eelco.de>
2021-12-03 13:58:46 +0100

    Day 3 part 2

--- Day03/solution.go
+++ Day03/solution.go
@@ -32,6 +32,48 @@ func CalcRates(data []string) (int64, int64) {
 	return epsilon, gamma
 }
 
+func GeneratorRatings(data []string) (int64, int64) {
+	var co_bits string
+	var oxygen_bits string
+
+	for i := 0; i < len(data[0]); i++ {
+		oxygen_one_count := 0
+		co_one_count := 0
+		oxygen_zero_count := 0
+		co_zero_count := 0
+		for j := 0; j < len(data); j++ {
+			if strings.HasPrefix(data[j], co_bits) {
+				if data[j][i] == '1' {
+					co_one_count++
+				} else {
+					co_zero_count++
+				}
+			}
+			if strings.HasPrefix(data[j], oxygen_bits) {
+				if data[j][i] == '1' {
+					oxygen_one_count++
+				} else {
+					oxygen_zero_count++
+				}
+			}
+		}
+		if oxygen_one_count >= oxygen_zero_count {
+			oxygen_bits += "1"
+		} else {
+			oxygen_bits += "0"
+		}
+		if (co_one_count < co_zero_count && co_one_count > 0) || co_zero_count == 0 {
+			co_bits += "1"
+		} else {
+			co_bits += "0"
+		}
+	}
+
+	co, _ := strconv.ParseInt(co_bits, 2, 64)
+	oxygen, _ := strconv.ParseInt(oxygen_bits, 2, 64)
+	return co, oxygen
+}
+
 func ReadFile(filename string) ([]string, error) {
 	fileBytes, err := ioutil.ReadFile(filename)
 
--- Day03/solution_test.go
+++ Day03/solution_test.go
@@ -22,3 +22,21 @@ func Test_Day3_Part1(t *testing.T) {
 	epsilon, gamma = CalcRates(real_data)
 	fmt.Printf("Solution part 1:\neps:%d gamma:%d\nProduct: %d\n", epsilon, gamma, epsilon*gamma)
 }
+
+func Test_Day3_Part2(t *testing.T) {
+	test_data, err := ReadFile("test_data.txt")
+	if err != nil {
+		t.Fatalf("Error opening test file: %v", err)
+	}
+	co, oxygen := GeneratorRatings(test_data)
+	if co != 10 || oxygen != 23 {
+		t.Fatalf("Expected co: 10 oxygen: 23\nGot: co:%d oxygen:%d", co, oxygen)
+	}
+
+	real_data, err := ReadFile("input.txt")
+	if err != nil {
+		t.Fatalf("Error opening test file: %v", err)
+	}
+	co, oxygen = GeneratorRatings(real_data)
+	fmt.Printf("\n\nSolution part 2:\nco:%d oxygen:%d\nProduct: %d\n\n", co, oxygen, co*oxygen)
+}
